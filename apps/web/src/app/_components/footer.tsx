c977282c1cd664e1e00c2530c478223ba8b0fb39
Nick Papadakis <nickfin2014@gmail.com>
2025-01-10 16:34:41 -0700

    revise copy right on footer

--- apps/web/src/app/_components/footer.tsx
+++ apps/web/src/app/_components/footer.tsx
@@ -68,7 +68,8 @@ export function Footer() {
                     </div>
 
                     <div className="mt-2 ml-2">
-                        &copy; copyright Startup 2024. All rights reserved.
+                        &copy; copyright Poes Pursuits LLC 2024. All rights
+                        reserved.
                     </div>
                 </div>
                 <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 items-start mt-10 sm:mt-0 md:mt-0">
