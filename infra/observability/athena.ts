e73ce2787d1ac6795f7beb4acc65de6f27c62fcb
Nick Papadakis <nickfin2014@gmail.com>
2025-01-30 21:29:36 -0700

    initiate observability spike ticket

---
+++ infra/observability/athena.ts
@@ -0,0 +1,7 @@
+// athena workgroup
+
+// athena "from glue" data catalog that references glue
+
+// athena database (S3 bucket) to store result of queries, maybe
+
+// athena queries, maybe.
---
+++ infra/observability/firehose.ts
@@ -0,0 +1,3 @@
+// fireHose /w S3 bucket as target, serialization to Parquett
+
+// policy to move data to glacier after X days
