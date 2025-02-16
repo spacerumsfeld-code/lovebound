24df630fea38671cb7a335ec9e0cb1b05fd3da54
Nick Papadakis <nickfin2014@gmail.com>
2025-02-16 14:52:00 -0700

    aws provider added via sst add so lets see

--- sst.config.ts
+++ sst.config.ts
@@ -1,26 +1,20 @@
 import type {} from './.sst/platform/config'
-
 import { readdirSync } from 'fs'
-
 export default $config({
     app(input) {
         return {
             name: 'lovebound-io',
             home: 'aws',
             removal: input?.stage === 'production' ? 'retain' : 'remove',
-            aws: {
-                version: '6.67.0',
-            },
+            providers: { aws: '6.68.0' },
         }
     },
     async run() {
         const outputs = {}
-
         for (const value of readdirSync('./infra/')) {
             const result = await import('./infra/' + value)
             if (result.outputs) Object.assign(outputs, result.outputs)
         }
-
         return outputs
     },
 })
